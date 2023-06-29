b37979317b658dd239a11cd9d619daf7be4a320f
Max Espinosa <maxespinosa@Maxs-MacBook-Air.local>
2023-06-29 12:04:07 -0400

    FULLY FUNCTIONING COFFEE MENU

---
+++ src/components/Coffees.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+
+
+export default function Coffees () {
+    const[coffeeList, setCoffeeList] = useState()
+    const [message, setMessage] = useState('Click a button above to get started!')
+
+    const getCoffee = async (type) => {
+        setMessage(`Loading ${type} coffees...`)
+        setCoffeeList()
+        const response = await fetch(`https://api.sampleapis.com/coffee/${type}`)
+        const data = await response.json()
+        setCoffeeList(data)
+    }
+     return (
+        <main>
+            <div className="button-container">
+            <button onClick={() => getCoffee('hot')}>Hot</button>
+            <button onClick={() => getCoffee('iced')}>Iced</button>
+            </div>
+            <div className="coffee-list">
+
+        {!coffeeList 
+        ? <p className="message">{message}</p>
+        : coffeeList.map(coffee => (
+            <div key={coffee.div} className="coffeeCard">
+                <h2>{coffee.title}</h2>
+                <div className="coffeeCardImage"
+                style={{backgroundImage: `url(${coffee.image})`}}
+                />
+            </div>
+        ))
+        }
+        </div>
+        </main>
+    )
+}
